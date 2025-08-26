eb3a70b9346878d40df5dcff442ed783c9575186
juniorsilvaeng <juniorsilvacc@hotmail.com>
2025-08-26 19:16:06 -0300

    conectando ao database

---
+++ fast_car_api/database.py
@@ -0,0 +1,23 @@
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+# URL do banco
+DATABASE_URL = 'sqlite:///cars.db'
+
+# Cria o motor de conexão com o banco
+engine = create_engine(DATABASE_URL)
+
+# Configura de sessões para interagir com o banco
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+# Declara tabelas do meu banco de dados
+Base = declarative_base()
+
+# Dependência para obter a sessão e garantir que será fechada no fim
+def get_session():
+    session = SessionLocal()
+    try:
+        yield session   # fornece a sessão para uso
+    finally:
+        session.close() # garante que fecha depois
