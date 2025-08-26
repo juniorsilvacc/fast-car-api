630bdc57987704b22f968edb96f9d03ffef26683
juniorsilvaeng <juniorsilvacc@hotmail.com>
2025-08-26 11:03:56 -0300

    estrutura inicial do projeto

---
+++ fast_car_api/app.py
@@ -0,0 +1,11 @@
+from fastapi import FastAPI
+
+app = FastAPI(
+    title='Fast Car API',
+    description='Modern Car API',
+    version='0.1.0'
+)
+
+@app.get('/carros')
+def read_root():
+    return {'status': 'ok'}
